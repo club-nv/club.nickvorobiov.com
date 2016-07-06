315442f5b035deb3e8cb1a2947ed643592fb8251
Nick Vorobiov <schaman@yandex.ru>
2016-07-06 17:16:22 +0300

    новая механика, с заявками

--- js/main.js
+++ js/main.js
@@ -19,16 +19,18 @@ $(function() {
 
 	// sticker
 
-	var about_bottom = $('.s-about').offset().top + $('.s-about').outerHeight(true);
-	var page_height = $('body').height();
-	
-	if (!device.mobile() && !device.tablet()) {
-		$(".js-sticker-about").sticky({
-			topSpacing: 0,
-			bottomSpacing: page_height - about_bottom,
-			zIndex: -1,
-		});
-	}
+  if ($('.s-about').length) {
+  	var about_bottom = $('.s-about').offset().top + $('.s-about').outerHeight(true);
+  	var page_height = $('body').height();
+  	
+  	if (!device.mobile() && !device.tablet()) {
+  		$(".js-sticker-about").sticky({
+  			topSpacing: 0,
+  			bottomSpacing: page_height - about_bottom,
+  			zIndex: -1,
+  		});
+  	}
+  }
 
 	// Smooth scroll
 
@@ -37,6 +39,63 @@ $(function() {
 		var href = $(this).attr("href");
 		var offsetTop = href === "#" ? 0 : $(href).offset().top-50;
 		$('html, body').stop().animate({ scrollTop: offsetTop}, 700);
-	});  
+	});
+
+  // form
+
+  $(document).on('submit', 'form', function (e){
+    //console.log('form: отправляю..');
+    var msg = $(this).serialize();
+
+    var $vk = $(this).find('[name=vk]');
+
+    var post_params = {
+      vk: $vk.val(),
+    };
+
+    if(post_params.vk !== undefined && !post_params.vk.length) {
+      $vk.addClass('error');
+      e.preventDefault();
+      return message.error('Укажи ссылку на свою страницу ВКонтакте, пожалуйста!', 'warning');
+    }
+  });
+
+  // MESSAGE
+
+  var message = {
+    error:  function (msg) {
+      var cls = 'warning';
+      message.init(msg,cls);
+    },
+    success:  function (msg) {
+      var cls = 'success';
+      message.init(msg,cls);
+    },
+    init: function(msg, cls) {
+      $body.append('<div class="notice notice--'+cls
+        +'" style="opacity:0;"><div class="wrapper">'+msg+'</div></div>');
+
+      var height_notice = $('.notice').height();
+      $('.notice').css('bottom', '-'+height_notice+'px');
+      $('.notice').animate({
+          bottom: 0,
+          opacity: 1
+      }, 300);
+
+      message.close(3000);
+      return false;
+    },
+    close: function(time) {
+      function notice_hide() {
+        $(".notice").animate({
+          opacity: 0},
+          500, function() {
+            $(this).remove();
+        });
+        $('.input').removeClass('error');
+      }
+      setTimeout(notice_hide, time);
+    }
+  };
 });
 
