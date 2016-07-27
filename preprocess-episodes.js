21d6388e9b7c0cab3ff2b0026cac4c231821914a
Nick Vorobiov <schaman@yandex.ru>
2016-07-27 22:49:19 +0300

    эпизоды разложены по папочкам категорий

--- preprocess-episodes.js
+++ preprocess-episodes.js
@@ -35,58 +35,63 @@ try {
   fs.mkdirSync(dstdir);
 }
 
-fs.readdirSync(srcdir).forEach(function(filename) {
-  if (filename.match(/\d\d\d\d-\d\d-\d\d-.*\.md/)) {
-    console.log(filename);
-    var date = filename.substring(0, 10);
-    var slug = filename.substring(11, filename.length - 3);
-    var dir = dstdir + '/' + date + '-' + slug;
+fs.readdirSync(srcdir).forEach(function(catfoldername) {
+  var catdir = srcdir + '/' + catfoldername;
+  if (fs.lstatSync(catdir).isDirectory()) {
+    fs.readdirSync(catdir).forEach(function(filename) {
+      if (filename.match(/\d\d\d\d-\d\d-\d\d-.*\.md/)) {
+        console.log(filename);
+        var date = filename.substring(0, 10);
+        var slug = filename.substring(11, filename.length - 3);
+        var dir = dstdir + '/' + date + '-' + slug;
 
-    try {
-      fs.accessSync(dir, fs.F_OK);
-    } catch (e) {
-      fs.mkdirSync(dir);
-    }
+        try {
+          fs.accessSync(dir, fs.F_OK);
+        } catch (e) {
+          fs.mkdirSync(dir);
+        }
 
-    var doc = load(srcdir + '/' + filename);
+        var doc = load(catdir + '/' + filename);
 
-    var links = {
-      listbuilder: '/listbuilder/' + slug + '/',
-      ok: '/listbuilder-ok/' + slug + '/',
-      enjoy: '/enjoy/' + slug + '/' + doc.event + '/',
-    }
+        var links = {
+          listbuilder: '/listbuilder/' + slug + '/',
+          ok: '/listbuilder-ok/' + slug + '/',
+          enjoy: '/enjoy/' + slug + '/' + doc.event + '/',
+        }
 
-    var listbuilder = {
-      title: doc.title,
-    }
+        var listbuilder = {
+          title: doc.title,
+        }
 
-    if (doc.introtext) listbuilder.introtext = doc.introtext;
-    if (doc.program) listbuilder.program = doc.program;
-    if (doc.gains) listbuilder.gains = doc.gains;
+        if (doc.introtext) listbuilder.introtext = doc.introtext;
+        if (doc.program) listbuilder.program = doc.program;
+        if (doc.gains) listbuilder.gains = doc.gains;
 
-    listbuilder.permalink = links.listbuilder;
-    listbuilder.redirect = links.ok;
-    listbuilder.layout = 'listbuilder';
+        listbuilder.permalink = links.listbuilder;
+        listbuilder.redirect = links.ok;
+        listbuilder.layout = 'listbuilder';
 
-    save(dir + '/listbuilder.md', listbuilder);
+        save(dir + '/listbuilder.md', listbuilder);
 
-    save(dir + '/listbuilder-ok.md', {
-      permalink: links.ok,
-      redirect: links.enjoy,
-      layout: 'listbuilder-ok'
-    });
+        save(dir + '/listbuilder-ok.md', {
+          permalink: links.ok,
+          redirect: links.enjoy,
+          layout: 'listbuilder-ok'
+        });
 
-    save(dir + '/enjoy.md', {
-      title: doc.title,
-      stream: doc.stream,
-      event: doc.event,
-      permalink: links.enjoy,
-      layout: 'enjoy'
-    });
+        save(dir + '/enjoy.md', {
+          title: doc.title,
+          stream: doc.stream,
+          event: doc.event,
+          permalink: links.enjoy,
+          layout: 'enjoy'
+        });
 
-    save(dir + '/summary.md', {
-      title: 'Конспект КЛР ' + date + ' ' + doc.title,
-      layout: 'summary'
-    }, doc.content);
+        save(dir + '/summary.md', {
+          title: 'Конспект КЛР ' + date + ' ' + doc.title,
+          layout: 'summary'
+        }, doc.content);
+      }
+    })
   }
 })
